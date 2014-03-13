af566d606fa31fe381de7f66975253a62d903859
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 17:57:04 -0600

    Added a sound that plays when you walk into a wall.

---
+++ Assets/Script/WallHit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallHit : MonoBehaviour {
+	public GameObject bumpEmitter;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		print (1);
+		if (coll.gameObject.tag == "Player") {
+			print (2);
+			GameObject bump = Instantiate (bumpEmitter) as GameObject;
+			bump.transform.position = coll.ClosestPointOnBounds(transform.position);
+			bump.audio.Play();
+			Destroy (bump,5);
+		}
+	}
+}
