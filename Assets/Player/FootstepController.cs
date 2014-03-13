b3fe80ffa0a552ab4d0db5299ba3b0b4bfcf8d57
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 13:59:14 -0600

    Added footstep sounds, tuned move speed.

---
+++ Assets/Player/FootstepController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepController : MonoBehaviour {
+	public float delay;
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+		{
+			if(!audio.isPlaying)
+				audio.PlayDelayed(delay);
+		}
+	}
+}
