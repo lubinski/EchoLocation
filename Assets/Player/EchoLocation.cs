3705ea9284f818783b6d6a3aecf0c1825745614f
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 13:41:47 -0600

    Basic echolocation.

--- Assets/Player/EchoLocation.cs
+++ Assets/Player/EchoLocation.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class EchoLocation : MonoBehaviour {
 	public int range;
+	public GameObject soundPrefab;
+	public float delayFactor;
 
 	// Update is called once per frame
 	void Update () {
@@ -11,8 +13,15 @@ public class EchoLocation : MonoBehaviour {
     		RaycastHit hit = new RaycastHit();
     		if (Physics.Raycast (ray, out hit, range)) {
     	    	Debug.DrawLine (ray.origin, hit.point,Color.red,5f);
+				Vector3 rayVector = hit.point - ray.origin;
+
 				//use a child object to play click sound for consistency.
 				transform.FindChild("ClickEmitter").audio.Play();
+
+				//Instantiate echo to play at target location.
+				GameObject echo = Instantiate (soundPrefab) as GameObject;
+				echo.transform.position = hit.point;
+				echo.audio.PlayDelayed(rayVector.magnitude * delayFactor);
     		}
 		}
 	}
