96911a64fd5191095d0043c2b344207ba6e0c6b3
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 13:30:00 -0600

    Click sound on pressing e.

--- Assets/Player/EchoLocation.cs
+++ Assets/Player/EchoLocation.cs
@@ -11,7 +11,8 @@ public class EchoLocation : MonoBehaviour {
     		RaycastHit hit = new RaycastHit();
     		if (Physics.Raycast (ray, out hit, range)) {
     	    	Debug.DrawLine (ray.origin, hit.point,Color.red,5f);
-
+				//use a child object to play click sound for consistency.
+				transform.FindChild("ClickEmitter").audio.Play();
     		}
 		}
 	}
