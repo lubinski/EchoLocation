8101bcf9db1321ca07451a68aa39222a6193b67c
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 17:32:10 -0600

    Changed movement to use keyboard to help keep orientation.

---
+++ Assets/Player/KeyboardLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("LookLeft")) {
+			transform.Rotate(0,-90, 0);
+		}
+		if (Input.GetButtonDown ("LookRight")) {
+			transform.Rotate(0,90, 0);
+		}
+	}
+}
