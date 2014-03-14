a8acfcbff315299a8e622798cae1c8245792b349
Eric Lam <elam3@ualberta.ca>
2014-03-14 01:43:53 -0600

    Added in 3 triggers for 3 sounds
    the sound files being: moan, helpme, and warmer.

---
+++ Assets/Script/PlaySound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			audio.Play();
+			Destroy(gameObject, 5);
+		}
+	}
+}
