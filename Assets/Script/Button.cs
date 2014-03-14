9ba732ae0de88b8f38f297ec307b4660884c6e6f
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 18:48:22 -0600

    Added third room with button puzzle.

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			GameObject door = transform.parent.FindChild("door").gameObject;
+			door.audio.Play();
+			Destroy (door,2);
+		}
+	}
+}
