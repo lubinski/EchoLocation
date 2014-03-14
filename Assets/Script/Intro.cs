9252ab4788416751a9613e2ee3b01921a578b66b
Zach Lubinski <lubinski@ualberta.ca>
2014-03-14 02:17:38 -0600

    Added intro.

---
+++ Assets/Script/Intro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+	float wait = 10f;
+	bool stop;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		wait = wait + Time.time;
+	}
+	void Update()
+	{
+		if (Time.time > wait && !stop) {
+			player.transform.position = new Vector3(1.88707f,1.625f,-9.59f);
+			stop = true;
+		}
+	}
+}
