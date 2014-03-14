ebe13ffe0296ac3bc375cf2b5ea2b629481b40c2
Eric Lam <elam3@ualberta.ca>
2014-03-14 00:55:00 -0600

    Added More to the 5th Room (Long Room) and added new "Beep" sound

---
+++ Assets/Script/ChaseScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseScene : MonoBehaviour {
+	public GameObject enemy;
+	public GameObject startPosition;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			enemy.transform.position = startPosition.transform.position;
+			Destroy(gameObject);
+		}
+	}
+}
