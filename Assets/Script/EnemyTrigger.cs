eb43271432a44ac2e4bbaf5d707ca2e2a8740bb3
Zach Lubinski <lubinski@ualberta.ca>
2014-03-13 20:44:08 -0600

    Added monster to the level.

---
+++ Assets/Script/EnemyTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTrigger : MonoBehaviour {
+	public GameObject monster;
+	bool fired;
+	void OnTriggerEnter(Collider coll)
+	{
+		if (fired) return;
+		if (coll.gameObject.tag == "Player") {
+			monster.SetActive(true);
+			audio.Play ();
+			fired = true;
+		}
+	}
+}
