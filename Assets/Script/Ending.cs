e1b639a0c79d1a0ddb7367ee635d106b288d6686
Tanvir Sajed <tsajed@gmail.com>
2014-03-14 03:50:11 -0600

    added Ending script and ending game object to finish game

---
+++ Assets/Script/Ending.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ending : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			audio.Play();
+			//Destroy (ending,3);
+			audio.pitch = 0.5f;
+			//Debug.Log("collided");
+			float wait = 0f;
+			while (wait < 10f) {
+				wait = Time.deltaTime + wait;
+			}
+			Application.Quit();
+		}
+	}
+}
